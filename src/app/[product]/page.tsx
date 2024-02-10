0dce3336e7ce9eb2fcb48b204ace58a145a806d5
arshdev16 <arshisha2516@gmail.com>
2024-02-10 20:28:42 +0530

    product page

--- src/app/[product]/page.tsx
+++ src/app/[product]/page.tsx
@@ -1,95 +1,107 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 type Props = {};
 
 const Product = (props: Props) => {
   const router = useRouter();
-  
+  const [images, setImages] = useState({
+    img1: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/3396ee3c-08cc-4ada-baa9-655af12e3120/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+    img2: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/e44d151a-e27a-4f7b-8650-68bc2e8cd37e/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+    img3: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/44fc74b6-0553-4eef-a0cc-db4f815c9450/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+    img4: "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d3eb254d-0901-4158-956a-4610180545e5/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+  });
+
+  const [activeImg, setActiveImage] = useState(images.img1);
+
+  const [amount, setAmount] = useState(1);
+
   return (
-    <section className="text-gray-400 bg-gray-900 body-font overflow-hidden">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="lg:w-4/5 mx-auto flex flex-wrap">
-      <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400"/>
-      <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-        <h1 className="text-white text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
-        <div className="flex mb-4">
-          <span className="flex items-center">
-            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <span className="ml-3">4 Reviews</span>
-          </span>
-          <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-800 text-gray-500 space-x-2">
-            <a>
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>
-            </a>
-          </span>
+    <div className="flex flex-col my-5 justify-between lg:flex-row gap-16 lg:items-center">
+      <div className="flex flex-col gap-6 lg:w-2/4">
+        <Image
+          src={activeImg}
+          alt=""
+          className="w-3/4 aspect-square object-cover rounded-xl justify-self-center self-center"
+          width={200}
+          height={200}
+        />
+        <div className="flex flex-row justify-evenly h-24 items-center">
+          <Image
+            src={images.img1}
+            alt=""
+            className="w-16 h-16 md:w-24 md:h-24 mx-3 rounded-md cursor-pointer"
+            onClick={() => setActiveImage(images.img1)}
+            width={50}
+            height={50}
+          />
+          <Image
+            src={images.img2}
+            alt=""
+            className="w-16 h-16 md:w-24 md:h-24 mx-3 rounded-md cursor-pointer"
+            onClick={() => setActiveImage(images.img2)}
+            width={50}
+            height={50}
+          />
+          <Image
+            src={images.img3}
+            alt=""
+            className="w-16 h-16 md:w-24 md:h-24 mx-3 rounded-md cursor-pointer"
+            onClick={() => setActiveImage(images.img3)}
+            width={50}
+            height={50}
+          />
+          <Image
+            src={images.img4}
+            alt=""
+            className="w-16 h-16 md:w-24 md:h-24 mx-3 rounded-md cursor-pointer"
+            onClick={() => setActiveImage(images.img4)}
+            width={50}
+            height={50}
+          />
         </div>
-        <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
-        <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-800 mb-5">
-          <div className="flex">
-            <span className="mr-3">Color</span>
-            <button className="border-2 border-gray-800 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button className="border-2 border-gray-800 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-            <button className="border-2 border-gray-800 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-          </div>
-          <div className="flex ml-6 items-center">
-            <span className="mr-3">Size</span>
-            <div className="relative">
-              <select className="rounded border border-gray-700 focus:ring-2 focus:ring-indigo-900 bg-transparent appearance-none py-2 focus:outline-none focus:border-indigo-500 text-white pl-3 pr-10">
-                <option>SM</option>
-                <option>M</option>
-                <option>L</option>
-                <option>XL</option>
-              </select>
-              <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4" viewBox="0 0 24 24">
-                  <path d="M6 9l6 6 6-6"></path>
-                </svg>
-              </span>
-            </div>
-          </div>
+      </div>
+      {/* ABOUT */}
+      <div className="flex flex-col gap-4 lg:w-2/4 mx-8">
+        <div>
+          <span className=" text-violet-600 font-semibold">
+            Special Sneaker
+          </span>
+          <h1 className="text-3xl font-bold">Nike Invincible 3</h1>
         </div>
-        <div className="flex">
-          <span className="title-font font-medium text-2xl text-white">$58.00</span>
-          <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
-          <button className="rounded-full w-10 h-10 bg-gray-800 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-            </svg>
+        <p className="text-gray-700">
+          Con unammortizzazione incredibile per sostenerti in tutti i tuoi
+          chilometri, Invincible 3 offre un livello di comfort elevatissimo
+          sotto il piede per aiutarti a dare il massimo oggi, domani e oltre.
+          Questo modello incredibilmente elastico e sostenitivo, è pensato per
+          dare il massimo lungo il tuo percorso preferito e fare ritorno a casa
+          carico di energia, in attesa della prossima corsa.
+        </p>
+        <h6 className="text-2xl font-semibold">$ 199.00</h6>
+        <div className="flex flex-row items-center gap-12">
+          <div className="flex flex-row items-center">
+            <button
+              className="bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl"
+              onClick={() => setAmount((prev) => prev - 1)}
+            >
+              -
+            </button>
+            <span className="py-4 px-6 rounded-lg">{amount}</span>
+            <button
+              className="bg-gray-200 py-2 px-4 rounded-lg text-violet-800 text-3xl"
+              onClick={() => setAmount((prev) => prev + 1)}
+            >
+              +
+            </button>
+          </div>
+          <button className="bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full">
+            Add to Cart
           </button>
         </div>
       </div>
     </div>
-  </div>
-</section>
   );
 };
 
--- src/components/Cards/CardOne.tsx
+++ src/components/Cards/CardOne.tsx
@@ -20,16 +20,16 @@ const CardOne = (props: Props) => {
           backgroundImage: `url(${imgUrl})`,
         }}
       ></div>
-      <div className="w-56 -mt-10 overflow-hidden rounded-b-lg shadow-lg md:w-64 bg-gray-900">
+      <div className="w-56 -mt-10 overflow-hidden rounded-lg shadow-lg md:w-64 bg-[#000814]">
         <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
           {name}
         </h3>
 
-        <div className="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+        <div className="flex items-center justify-between px-3 py-2 bg-[#001d3d]">
           <span className="font-bold text-gray-800 dark:text-gray-200">
             {price}/-
           </span>
-          <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">
+          <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-[#6200b3] rounded focus:outline-none">
             Add to
           </button>
         </div>
